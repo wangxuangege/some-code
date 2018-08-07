79400e27ad74504ce7454dcb3099e56b4aa964fd
xqhuang <xqhuang@chinaums.com>
2018-08-07 16:34:07 +0800

    constructor使用method invoke调用

--- mybatis/src/test/java/com/wx/somecode/RuleTest.java
+++ mybatis/src/test/java/com/wx/somecode/RuleTest.java
@@ -1,20 +1,28 @@
 package com.wx.somecode;
 
+import org.junit.Test;
+
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author xinquan.huangxq
  */
-public class TestConstructor {
+public class RuleTest {
 
-    public static void main(String[] args) throws Throwable {
+    @Test
+    public void test1() throws Throwable {
         final Constructor<MethodHandles.Lookup> constructor = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, int.class);
         if (!constructor.isAccessible()) {
             constructor.setAccessible(true);
         }
 
-        Rule c = new Rule() {{
+        Rule c = new Rule() {
+            {
                 setId(2);
             }
 
@@ -37,4 +45,28 @@ public class TestConstructor {
                 MethodHandles.Lookup.PRIVATE | MethodHandles.Lookup.PROTECTED | MethodHandles.Lookup.PACKAGE | MethodHandles.Lookup.PUBLIC)
                 .unreflectSpecial(c.getClass().getMethod("getId"), c.getClass()).bindTo(c).invokeWithArguments());
     }
+
+    @Test
+    public void test2() throws Throwable {
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        MethodHandle mh = lookup.findStatic(RuleTest.class, "doubleVal", MethodType.methodType(int.class, int.class));
+        List<Integer> dataList = Arrays.asList(1, 2, 3, 4, 5);
+        RuleTest.transform(dataList, mh);
+        for (Integer data : dataList) {
+            System.out.println(data);
+        }
+    }
+
+    public static List<Integer> transform(List<Integer> dataList, MethodHandle handle) throws Throwable {
+        for (int i = 0; i < dataList.size(); i++) {
+            dataList.set(i, (Integer) handle.invoke(dataList.get(i)));
+        }
+        return dataList;
+    }
+
+    public static int doubleVal(int val) {
+        return val * 2;
+    }
+
+
 }

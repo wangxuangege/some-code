15592296ed3dfe9e9954819dcd2569279a7766e3
xqhuang <xqhuang@chinaums.com>
2018-08-31 13:01:40 +0800

    Build JSR注解实现

---
+++ jsr269/jsr269-main/src/main/java/com/wx/somecode/Order.java
@@ -0,0 +1,34 @@
+package com.wx.somecode;
+
+/**
+ * @author xinquan.huangxq
+ */
+@MyBuilder
+public class Order {
+    private long id;
+    private long addTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(long addTime) {
+        this.addTime = addTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", addTime=" + addTime +
+                '}';
+    }
+}

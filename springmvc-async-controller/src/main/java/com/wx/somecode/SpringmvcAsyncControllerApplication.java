026e3b00d87fc901d75c016131f8d4c1fe15ed32
wangxuan <WMI?wangxuan>
2019-01-24 10:29:01 +0800

    springmvc async controller

---
+++ springmvc-async-controller/src/main/java/com/wx/somecode/SpringmvcAsyncControllerApplication.java
@@ -0,0 +1,14 @@
+package com.wx.somecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcAsyncControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcAsyncControllerApplication.class, args);
+    }
+
+}
+
---
+++ springmvc-async-controller/src/main/java/com/wx/somecode/TestController.java
@@ -0,0 +1,68 @@
+package com.wx.somecode;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    /**
+     * SpringMVC调用Controller，Controller返回一个Callback对象
+     * SpringMVC调用ruquest.startAsync并且将Callback提交到TaskExecutor中去执行
+     * DispatcherServlet以及Filters等从应用服务器线程中结束，但Response仍旧是打开状态，也就是说暂时还不返回给客户端
+     * TaskExecutor调用Callback返回一个结果，SpringMVC将请求发送给应用服务器继续处理
+     * DispatcherServlet再次被调用并且继续处理Callback返回的对象，最终将其返回给客户端
+     *
+     * @return
+     */
+    @RequestMapping("/testCallable")
+    public Callable<String> testCallable() {
+        log.info("Controller开始执行！");
+        Callable<String> callable = () -> {
+            Thread.sleep(5000);
+
+            log.info("实际工作执行完成！");
+
+            return "succeed!";
+        };
+        log.info("Controller执行结束！");
+        return callable;
+    }
+
+    /**
+     * SpringMVC调用Controller，Controller返回一个DeferredResult对象
+     * SpringMVC调用ruquest.startAsync
+     * DispatcherServlet以及Filters等从应用服务器线程中结束，但Response仍旧是打开状态，也就是说暂时还不返回给客户端
+     * 某些其它线程将结果设置到DeferredResult中，SpringMVC将请求发送给应用服务器继续处理
+     * DispatcherServlet再次被调用并且继续处理DeferredResult中的结果，最终将其返回给客户端
+     *
+     */
+    private DeferredResult<String> deferredResult = new DeferredResult<>();
+
+    /**
+     * 返回DeferredResult对象
+     *
+     * @return
+     */
+    @RequestMapping("/testDeferredResult")
+    public DeferredResult<String> testDeferredResult() {
+        return deferredResult;
+    }
+
+    /**
+     * 对DeferredResult的结果进行设置
+     * @return
+     */
+    @RequestMapping("/setDeferredResult")
+    public String setDeferredResult() {
+        deferredResult.setResult("Test result!" + new Random().nextInt());
+        return "succeed";
+    }
+}

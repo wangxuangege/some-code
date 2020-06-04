d8929ea70ef74b5d90dac31a6bb4dea9bf08c3c8
xqhuang <xqhuang@chinaums.com>
2020-06-04 09:03:40 +0800

    pf4j demo

--- pf4j/pf4j-main/src/main/java/com/wx/somecode/pf4j/Main.java
+++ pf4j/pf4j-main/src/main/java/com/wx/somecode/pf4j/Main.java
@@ -1,7 +1,6 @@
 package com.wx.somecode.pf4j;
 
-import org.pf4j.DefaultPluginManager;
-import org.pf4j.PluginManager;
+import org.pf4j.*;
 
 import java.util.List;
 
@@ -11,15 +10,21 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        System.setProperty("pf4j.pluginsDir", "plugins");
+        System.setProperty("pf4j.pluginsDir", "D:\\Workspace\\projects\\some-code\\pf4j\\pf4j-main\\target\\classes\\plugins");
 
-        final PluginManager pluginManager = new DefaultPluginManager();
+        final PluginManager pluginManager = new DefaultPluginManager() {
+            protected ExtensionFinder createExtensionFinder() {
+                DefaultExtensionFinder extensionFinder = (DefaultExtensionFinder) super.createExtensionFinder();
+                return extensionFinder;
+            }
+        };
 
         pluginManager.loadPlugins();
 
         pluginManager.startPlugins();
 
         List<Greeting> greetings = pluginManager.getExtensions(Greeting.class);
+
         System.out.println(String.format("Found %d extensions for extension point '%s'", greetings.size(), Greeting.class.getName()));
         for (Greeting greeting : greetings) {
             System.out.println(">>> " + greeting.getGreeting());
